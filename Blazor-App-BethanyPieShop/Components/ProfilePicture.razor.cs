e45d26f34c62eb604e2acd98e607181c47f2d910
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-02 10:26:46 -0600

    commit

---
+++ Blazor-App-BethanyPieShop/Components/ProfilePicture.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorBethanyPieShop.App.Components
+{
+	public partial class ProfilePicture
+	{
+
+		[Parameter]
+		public RenderFragment? ChildContent { get; set; }
+
+
+
+
+	}
+}
---
+++ Blazor-App-BethanyPieShop/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using BlazorBethanyPieShop.App.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBethanyPieShop.App.Pages
+{
+	public partial class EmployeeDetail
+	{
+
+		[Parameter]
+		public string EmployeeId { get; set; }
+
+
+		public Employee? Employee { get; set; } = new Employee();
+
+
+		protected override Task OnInitializedAsync()
+		{
+
+			Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+
+			return base.OnInitializedAsync();
+		}
+
+
+	}
+}
