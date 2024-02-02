abf919241b01a7364a6fc8720cbfc818262a29b9
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-01 18:05:32 -0600

    commit

---
+++ Blazor-App-BethanyPieShop/Components/EmployeeCard.razor.cs
@@ -0,0 +1,12 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorBethanyPieShop.App.Components
+{
+	public partial class EmployeeCard
+	{
+		[Parameter]
+		public Employee Employee { get; set; } = default!;
+
+	}
+}
---
+++ Blazor-App-BethanyPieShop/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,17 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using BlazorBethanyPieShop.App.Models;
+
+namespace BlazorBethanyPieShop.App.Pages
+{
+	public partial class EmployeeOverview
+	{
+
+		public List<Employee>? Employees { get; set; } = default!;
+
+		protected override void OnInitialized()
+		{
+			Employees = MockDataService.Employees;
+		}
+
+	}
+}
